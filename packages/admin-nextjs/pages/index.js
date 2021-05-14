ece41b746d9bd83cae35831b2ff760d5ea5b9652
Luis Mendes <machadoluismendes@gmail.com>
2021-05-14 09:52:31 -0300

    project setup

--- packages/admin-nextjs/pages/index.js
+++ packages/admin-nextjs/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World Trello Tasks with timedoctor trial.{' '}
       <Link href="/about">
         <a>About</a>
       </Link>
